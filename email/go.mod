a2260a3f992d83d63f735f39221e4dc5abad087e
m0-ar <s176492@student.dtu.dk>
2022-05-03 20:30:01 +0200

    Consuming Kafka Messages

--- email/go.mod
+++ email/go.mod
@@ -1,3 +1,5 @@
 module email
 
 go 1.13
+
+require github.com/confluentinc/confluent-kafka-go v1.8.2 // indirect
--- email/main.go
+++ email/main.go
@@ -1,6 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
 func main() {
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "pkc-4ygn6.europe-west3.gcp.confluent.cloud:9092",
+		"security.protocol": "SASL_SSL",
+		"sasl.username":     "63UVCVFVTBPESSWN",
+		"sasl.password":     "OxmD4lsSR8xT9CfQbjnLnaX3o1VX0W2qpyxP8YH8B/vPA4GlMHjgoV/bYTkx15Gx",
+		"sasl.mechanism":    "PLAIN",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	consumer.SubscribeTopics([]string{"default"}, nil)
+
+	for {
+		msg, err := consumer.ReadMessage(-1)
+		if err == nil {
+			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+		} else {
+			// The client will automatically try to recover from all errors.
+			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+		}
+	}
+
+	consumer.Close()
 	//ambassadorMessage := []byte(fmt.Sprintf("You earned $%f from the link #%s", ambassadorRevenue, order.Code))
 	//smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{order.AmbassadorEmail}, ambassadorMessage)
 	//
