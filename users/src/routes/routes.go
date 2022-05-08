ca23a40a18c757ef5d25b42b914e04a975241b28
m0-ar <s176492@student.dtu.dk>
2022-05-08 16:10:13 +0200

    Get authenticate user

--- users/src/routes/routes.go
+++ users/src/routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"users/src/controllers"
+	"users/src/middlewares"
 )
 
 func Setup(app *fiber.App) {
@@ -11,8 +12,8 @@ func Setup(app *fiber.App) {
 	api.Post("register", controllers.Register)
 	api.Post("login", controllers.Login)
 
-	//authenticated := api.Use(middlewares.IsAuthenticated)
-	//authenticated.Get("user", controllers.User)
+	authenticated := api.Use(middlewares.IsAuthenticated)
+	authenticated.Get("user", controllers.User)
 	//authenticated.Post("logout", controllers.Logout)
 	//authenticated.Put("users/info", controllers.UpdateInfo)
 	//authenticated.Put("users/password", controllers.UpdatePassword)
