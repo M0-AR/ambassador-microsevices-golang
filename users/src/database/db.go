12bba40db0b97be1385c3fea80447675163abecf
m0-ar <s176492@student.dtu.dk>
2022-05-22 19:25:29 +0200

    Successfully deployed the user

--- users/src/database/db.go
+++ users/src/database/db.go
@@ -1,8 +1,10 @@
 package database
 
 import (
+	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"os"
 	"users/src/models"
 )
 
@@ -11,7 +13,15 @@ var DB *gorm.DB
 func Connect() {
 	var err error
 
-	DB, err = gorm.Open(mysql.Open("root:root@tcp(users_db:3306)/users"), &gorm.Config{})
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	database := os.Getenv("DB_DATABASE")
+	username := os.Getenv("DB_USERNAME")
+	password := os.Getenv("DB_PASSWORD")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, host, port, database)
+
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic("Could not connect with the database!")
