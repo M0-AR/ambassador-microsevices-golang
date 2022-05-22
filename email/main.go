1c9afaa8bfa91709082eba39d875132e2ebe1cbd
m0-ar <s176492@student.dtu.dk>
2022-05-22 13:07:33 +0200

    Production yaml for email

--- email/main.go
+++ email/main.go
@@ -5,15 +5,16 @@ import (
 	"fmt"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"net/smtp"
+	"os"
 )
 
 func main() {
 	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers": "pkc-4ygn6.europe-west3.gcp.confluent.cloud:9092",
-		"security.protocol": "SASL_SSL",
-		"sasl.username":     "63UVCVFVTBPESSWN",
-		"sasl.password":     "OxmD4lsSR8xT9CfQbjnLnaX3o1VX0W2qpyxP8YH8B/vPA4GlMHjgoV/bYTkx15Gx",
-		"sasl.mechanism":    "PLAIN",
+		"bootstrap.servers": os.Getenv("BOOTSTRAP_SERVERS"),
+		"security.protocol": os.Getenv("SECURITY_PROTOCOL"),
+		"sasl.username":     os.Getenv("SASL_USERNAME"),
+		"sasl.password":     os.Getenv("SASL_PASSWORD"),
+		"sasl.mechanism":    os.Getenv("SASL_MECHANISM"),
 		"group.id":          "myGroup",
 		"auto.offset.reset": "earliest",
 	})
@@ -22,13 +23,11 @@ func main() {
 		panic(err)
 	}
 
-	consumer.SubscribeTopics([]string{"email_topic"}, nil)
+	consumer.SubscribeTopics([]string{os.Getenv("KAFKA_TOPIC")}, nil)
 
 	for {
 		msg, err := consumer.ReadMessage(-1)
-		if err == nil {
-			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
-		} else {
+		if err != nil {
 			// The client will automatically try to recover from all errors.
 			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
 			return
@@ -40,13 +39,16 @@ func main() {
 
 		json.Unmarshal(msg.Value, &message)
 
+		host := os.Getenv("EMAIL_HOST")
+		port := os.Getenv("EMAIL_PORT")
+
+		auth := smtp.PlainAuth("", os.Getenv("EMAIL_USERNAME"), os.Getenv("EMAIL_PASSWORD"), host)
+
 		ambassadorMessage := []byte(fmt.Sprintf("You earned $%f from the link #%s", message["ambassador_revenue"].(float64), message["code"]))
-		//smtp.SendMail("172.17.0.1:1025", nil, "no-reply@email.com", []string{message["ambassador_email"].(string)}, ambassadorMessage) // TODO: instead of 172.17.0.1 could be host.docker.internal
-		smtp.SendMail("0.0.0.0:1025", nil, "no-reply@email.com", []string{message["ambassador_email"].(string)}, ambassadorMessage) // TODO: instead of 172.17.0.1 could be host.docker.internal
+		smtp.SendMail(host+":"+port, auth, "no-reply@email.com", []string{message["ambassador_email"].(string)}, ambassadorMessage) // TODO: instead of 172.17.0.1 could be host.docker.internal
 
 		adminMessage := []byte(fmt.Sprintf("Order #%d with a total of $%f has been completed", message["id"].(float64), message["admin_revenue"].(float64))) // TODO: instead of 172.17.0.1 could be host.docker.internal
-		//smtp.SendMail("172.17.0.1:1025", nil, "no-reply@email.com", []string{"admin@admin.com"}, adminMessage)
-		smtp.SendMail("0.0.0.0:1025", nil, "no-reply@email.com", []string{"admin@admin.com"}, adminMessage)
+		smtp.SendMail(host+":"+port, auth, "no-reply@email.com", []string{"admin@admin.com"}, adminMessage)
 	}
 
 	consumer.Close()
